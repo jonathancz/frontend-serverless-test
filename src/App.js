e0199e93010fca7ed04de076361f53ee21a99c3b
Jonathan Chen <ethan.chen@covenant.edu>
2020-08-03 11:06:57 -0400

    Add Positive and Negative buttons

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import ReactDOM from 'react-dom'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid';
+
+import DeleteIcon from '@material-ui/icons/Delete';
+import Icon from '@material-ui/core/Icon';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,10 +17,14 @@ const useStyles = makeStyles((theme) => ({
       width: 200,
     },
   },
+  button: {
+    margin: theme.spacing(1),
+  },
 }));
 
 
 function App() {
+  const classes = useStyles()
   return (
     <div className="App">
       <form className={useStyles.root} noValidate autoComplete="off">
@@ -39,6 +47,29 @@ function App() {
       </div>
     </form>
     <div>
+    Was your feedback positive or negative?
+    <Grid id="top-row" container spacing={24} direction="row" justify="center" alignItems="center">
+      <Grid item xs={1}>
+        <Button
+        variant="contained"
+        color="primary"
+        className={classes.button}
+        >
+          Positive
+        </Button>
+      </Grid>
+      <Grid item xs={1}>
+      <Button
+          variant="contained"
+          color="secondary"
+          className={classes.button}
+        >
+          Negative
+      </Button>
+      </Grid>
+    </Grid>
+   
+
       <Button variant="contained" color="primary">
         Submit Feedback
       </Button>
