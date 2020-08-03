178f33b365d31d92e2cc9bba050f5a1f8677d063
Jonathan Chen <jonathanchenzheng@hotmail.com>
2020-08-02 23:29:44 -0400

    Install dependencies and import required components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import './App.css'
+import cx from 'clsx'
 import ReactDOM from 'react-dom'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardMedia from '@material-ui/core/CardMedia'
+import CardContent from '@material-ui/core/CardContent'
+import TextInfoContent from '@mui-treasury/components/content/textInfo'
+import { useBlogTextInfoContentStyles } from '@mui-treasury/styles/textInfoContent/blog'
+import { useOverShadowStyles } from '@mui-treasury/styles/shadow/over'
 
 const useStyles = makeStyles((theme) => ({
   root: {
