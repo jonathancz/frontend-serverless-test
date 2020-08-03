cd713e9ce510cf64084a3589b6d531020052f17a
Jonathan Chen <ethan.chen@covenant.edu>
2020-08-03 10:43:37 -0400

    Remove unused libraries

--- src/App.js
+++ src/App.js
@@ -5,12 +5,6 @@ import ReactDOM from 'react-dom'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import { makeStyles } from '@material-ui/core/styles'
-import Card from '@material-ui/core/Card'
-import CardMedia from '@material-ui/core/CardMedia'
-import CardContent from '@material-ui/core/CardContent'
-import TextInfoContent from '@mui-treasury/components/content/textInfo'
-import { useBlogTextInfoContentStyles } from '@mui-treasury/styles/textInfoContent/blog'
-import { useOverShadowStyles } from '@mui-treasury/styles/shadow/over'
 
 const useStyles = makeStyles((theme) => ({
   root: {
